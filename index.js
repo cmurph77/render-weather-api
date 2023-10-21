775fea8a744a68f0903451443bda21774bc7283e
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-21 15:58:05 +0100

    testing adding a new endpoint

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.get('/test-new-endpoint', async (req, res) => {
+  const username = req.query.username || 'myogeshchavan97';
+  try {
+    const reply = {
+      message :  'great success'
+    }
+    
+    res.send(reply);
+  } catch (error) {
+    res.status(400).send('Error while getting list of repositories');
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
