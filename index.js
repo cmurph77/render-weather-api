e1a1ef86aa5ac84d0f3c6e1aecf0c5dd7ab9943c
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-21 15:45:56 +0100

    created simple get endpoint

--- index.js
+++ index.js
@@ -6,19 +6,11 @@ const PORT = process.env.PORT || 8081;
 app.get('/', async (req, res) => {
   const username = req.query.username || 'myogeshchavan97';
   try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
-
-    res.send(repos);
+    const reply = {
+      hello :  'world'
+    }
+    
+    res.send(reply);
   } catch (error) {
     res.status(400).send('Error while getting list of repositories');
   }
