545a983a3604cda733283a2894f103c982e025e3
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-22 02:46:55 +0100

    deleted lines

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');  //import express to create api endpoints
-
-//const { json } = require('body-parser');
 const cors = require('cors'); // Import the cors middleware
 const { getWeatherForecastDaily } = require('./weather_api_call.js'); // import js file that handled retrieving weather data
 
@@ -9,61 +7,7 @@ const PORT = process.env.PORT || 8081;  // expose the port for the api to listen
 
 app.use(cors()); // enable CORS for all routes
 
-
-
-// const sample_data= {
-//   "city": "Sample Data City",
-//   "lon": 1000,
-//   "lat": 1000,
-//   "days" : {
-//       "day_one" : {
-//           "unix_dt": 100000000,
-//           "formated_date": "1.12.1999",
-//           "temp": 100 ,
-//           "rainfall_level": 12,
-//           "windspeed": 20 ,
-//           "weather_type": "COLD",
-//           "wear_mask" : true
-//       },
-//       "day_two" :{
-//           "unix_dt": 20000000,
-//           "formated_date": "2.12.1999",
-//           "temp": 23.4 ,
-//           "rainfall_level": 12,
-//           "windspeed": 20 ,
-//           "weather_type": "COLD",
-//           "wear_mask" : true
-//       },
-//       "day_three": {
-//           "unix_dt": 3000000,
-//           "formated_date": "3.12.1999",
-//           "temp": 23.4 ,
-//           "rainfall_level": 12,
-//           "windspeed": 20 ,
-//           "weather_type": "COLD",
-//           "wear_mask" : true
-//       },
-//       "day_four":{
-//           "unix_dt": 4000000,
-//           "formated_date": "4.12.1999",
-//           "temp": 23.4 ,
-//           "rainfall_level": 12,
-//           "windspeed": 20 ,
-//           "weather_type": "COLD",
-//           "wear_mask" : true
-//       },
-//       "day_five": {
-//           "unix_dt": 5000000,
-//           "formated_date": "5.12.1999",
-//           "temp": 23.4 ,
-//           "rainfall_level": 12,
-//           "windspeed": 20 ,
-//           "weather_type": "COLD",
-//           "wear_mask" : true
-//       }
-//   }
-// }
-           
+      
 
 
 /**
@@ -82,45 +26,6 @@ async function getdata(city) {
   }
 }
 
-// app.get('/', async (req, res) => {
-//   try {
-//     const reply = {
-//       message :  'great success'
-//     }
-    
-//     res.send(reply);
-//   } 
-//   catch (error) {
-//     res.status(400).send('Error while getting list of repositories');
-//   }
-// });
-
-// app.get('/sample', async (req, res) => {
-//   try {
-//     const reply = {
-//       message :  'great success'
-//     }
-//     console.log("RECIEVED SAMPLE DATA REQUEST")
-    
-//     res.send(sample_data);
-//   } 
-//   catch (error) {
-//     res.status(400).send('Error while getting list of repositories');
-//   }
-// });
-
-// app.get('/sample/:input', async (req, res) => {
-//   const input = req.params.input;
-//   try {
-
-//     console.log("RECIEVED SAMPLE DATA REQUEST")
-//     sample_data.city = input;
-//     res.send(sample_data);
-//   } 
-//   catch (error) {
-//     res.status(400).send('Error while fetching data');
-//   }
-// });
 
 /**
  * @brief rerturns the current polution data for a lat and long
@@ -165,7 +70,7 @@ app.get('/polution/:lat/:lon', async (req, res) => {
 
  });
 
- 
+
 /**
  * @brief This is a GET enpoint that takes the city name as an input and return the weather data for the city
  */
